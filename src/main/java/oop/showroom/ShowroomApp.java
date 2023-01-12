ab221614036e1bb363bd3d428c33b0c75841a88a
Aleksander Linczuk <aleksanderlinczuk@gmail.com>
2023-01-12 20:16:26 +0100

    showroom application ready

--- src/main/java/oop/showroom/ShowroomApp.java
+++ src/main/java/oop/showroom/ShowroomApp.java
@@ -12,20 +12,19 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ShowroomApp {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
 
     public static void main(String[] args) throws InterruptedException {
+        ShowroomApp showroomApp = new ShowroomApp();
+        showroomApp.run();
 
-        Car audi = new Car(BrandType.AUDI, ModelType.A4, ColourType.RED, EngineType.V8, 10000,
-                GearboxType.AUTOMATIC, false, FuelType.PB95, "car-drive", 5,
-                200, 5);
 
-        Motorbike bmw = new Motorbike(BrandType.BMW, ModelType.X6, ColourType.GREEN, EngineType.V8, 50000,
-                GearboxType.AUTOMATIC, true,MotorbikeType.CHOPPER, "łańcuch");
-
+    }
+    private void run() throws InterruptedException {
         VehicleService vehicleService = new VehicleServiceImpl();
+        init(vehicleService);
 
-        vehicleService.addVehicle(audi);
-        vehicleService.addVehicle(bmw);
 
 //        System.out.println(Arrays.toString(VehicleRepository.vehicles));
         Scanner scanner = new Scanner(System.in);
@@ -34,33 +33,72 @@ public class ShowroomApp {
         System.out.println();
         Thread.sleep(1000);
         while (on){
-
-            System.out.println("Jaką operację chcesz wykonać?");
-            System.out.println("1 - dodaj nowy samochód");
-            System.out.println("2 - dodaj nowy motocykl");
-            System.out.println("3 - wyświetl wszystkie pojazdy");
-            System.out.println("0 - zakończ program");
             System.out.println("Wpisz odpowiednią liczbę:");
-            int decision = scanner.nextInt();
+            int decision = SCANNER.nextInt();
 
-            switch (decision){
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    System.out.println("Lista wszystkich pojazdów:");
-                    for (Vehicle vehicle : vehicleService.getAllVehicles()) {
-                        System.out.println(vehicle);
-                    }
-                    break;
-                case 0:
+            switch (decision) {
+                case 1 -> addCar(vehicleService);
+                case 2 -> addMotorbike(vehicleService);
+                case 3 -> showVehicles(vehicleService);
+                case 0 -> {
                     on = false;
                     System.out.println("Do widzenia!");
-                    break;
-                default:
-                    System.out.println("Nie ma operacji o takim numerze");
+                }
+                default -> System.out.println("Nie ma operacji o takim numerze");
             }
         }
+    }
+
+
+
+
+    private void showMenu(){
+        System.out.println("Jaką operację chcesz wykonać?");
+        System.out.println("1 - dodaj nowy samochód");
+        System.out.println("2 - dodaj nowy motocykl");
+        System.out.println("3 - wyświetl wszystkie pojazdy");
+        System.out.println("0 - zakończ program");
+    }
+
+    private void addCar(VehicleService vehicleService) {
+        Car car = new Car();
+        System.out.println("Podaj markę: BMW, AUDI, CITROEN");
+        String brand = SCANNER.next();
+        car.setBrand(BrandType.valueOf(brand));
+        System.out.println("Podaj liczbę drzwi:");
+        int doorsNumber = SCANNER.nextInt();
+        car.setDoorsNumber(doorsNumber);
+        vehicleService.addVehicle(car);
+        System.out.println("Dodano samochód!");
+    }
+
+    private void addMotorbike(VehicleService vehicleService) {
+        Motorbike motorbike = new Motorbike();
+        System.out.println("Podaj markę");
+        String motorbikeBrand = SCANNER.next();
+        motorbike.setBrand(BrandType.valueOf(motorbikeBrand));
+        System.out.println("podaj rodzaj napędu:");
+        String motorbikeDrive = SCANNER.next();
+        motorbike.setMotorbikeDrive(motorbikeDrive);
+        vehicleService.addVehicle(motorbike);
+        System.out.println("Motor dodany");
+    }
+
+    private void showVehicles(VehicleService vehicleService){
+        System.out.println("Lista wszystkich pojazdów:");
+        for (Vehicle vehicle : vehicleService.getAllVehicles()) {
+            System.out.println(vehicle);
+        }
+    }
+    private void init(VehicleService vehicleService){
+
+        Car audi = new Car(BrandType.AUDI, ModelType.A4, ColourType.RED, EngineType.V8, 10000,
+                GearboxType.AUTOMATIC, false, FuelType.PB95, "car-drive", 5,
+                200, 5);
+
+        Motorbike bmw = new Motorbike(BrandType.BMW, ModelType.X6, ColourType.GREEN, EngineType.V8, 50000,
+                GearboxType.AUTOMATIC, true,MotorbikeType.CHOPPER, "łańcuch");
+        vehicleService.addVehicle(audi);
+        vehicleService.addVehicle(bmw);
     }
 }
