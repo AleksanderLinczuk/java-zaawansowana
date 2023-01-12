bb9b753759c81f509385d9c16596a0949d475879
Aleksander Linczuk <aleksanderlinczuk@gmail.com>
2023-01-12 19:23:39 +0100

    showroom application

---
+++ src/main/java/oop/showroom/ShowroomApp.java
@@ -0,0 +1,66 @@
+package oop.showroom;
+
+import oop.showroom.model.Car;
+import oop.showroom.model.Motorbike;
+import oop.showroom.model.Vehicle;
+import oop.showroom.model.enums.*;
+import oop.showroom.repository.VehicleRepository;
+import oop.showroom.service.VehicleService;
+import oop.showroom.service.VehicleServiceImpl;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ShowroomApp {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Car audi = new Car(BrandType.AUDI, ModelType.A4, ColourType.RED, EngineType.V8, 10000,
+                GearboxType.AUTOMATIC, false, FuelType.PB95, "car-drive", 5,
+                200, 5);
+
+        Motorbike bmw = new Motorbike(BrandType.BMW, ModelType.X6, ColourType.GREEN, EngineType.V8, 50000,
+                GearboxType.AUTOMATIC, true,MotorbikeType.CHOPPER, "łańcuch");
+
+        VehicleService vehicleService = new VehicleServiceImpl();
+
+        vehicleService.addVehicle(audi);
+        vehicleService.addVehicle(bmw);
+
+//        System.out.println(Arrays.toString(VehicleRepository.vehicles));
+        Scanner scanner = new Scanner(System.in);
+        boolean on = true;
+        System.out.println("Witaj w aplikacji salonu pojazdów!");
+        System.out.println();
+        Thread.sleep(1000);
+        while (on){
+
+            System.out.println("Jaką operację chcesz wykonać?");
+            System.out.println("1 - dodaj nowy samochód");
+            System.out.println("2 - dodaj nowy motocykl");
+            System.out.println("3 - wyświetl wszystkie pojazdy");
+            System.out.println("0 - zakończ program");
+            System.out.println("Wpisz odpowiednią liczbę:");
+            int decision = scanner.nextInt();
+
+            switch (decision){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    System.out.println("Lista wszystkich pojazdów:");
+                    for (Vehicle vehicle : vehicleService.getAllVehicles()) {
+                        System.out.println(vehicle);
+                    }
+                    break;
+                case 0:
+                    on = false;
+                    System.out.println("Do widzenia!");
+                    break;
+                default:
+                    System.out.println("Nie ma operacji o takim numerze");
+            }
+        }
+    }
+}
---
+++ src/main/java/oop/showroom/service/VehicleServiceImpl.java
@@ -0,0 +1,37 @@
+package oop.showroom.service;
+
+import oop.showroom.model.Vehicle;
+import oop.showroom.repository.VehicleRepository;
+
+public class VehicleServiceImpl implements VehicleService{
+
+    static int  id = 0;
+    @Override
+    public void addVehicle(Vehicle vehicle) {
+        VehicleRepository.vehicles[id] = vehicle;
+        id++;
+    }
+
+    @Override
+    public Vehicle[] getAllVehicles() {
+
+
+        Vehicle[] tab = VehicleRepository.vehicles;
+        int counter = 0;
+
+        for (int i =0; i<tab.length; i++){
+            if (tab[i]!= null){
+                counter++;
+            }
+        }
+        Vehicle[] result = new Vehicle[counter];
+        int index = 0;
+        for (Vehicle v:tab){
+            if (v!= null){
+                result[index] = v;
+                index++;
+            }
+        }
+        return result;
+    }
+}
